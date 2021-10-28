5eecf6ee3e6fa0a930ac6032ca54a2fab121d228
jjia <972931271@qq.com>
2021-10-28 17:32:05 +0200

    replace myutil by medutils

--- ssc_scoring/compute_metrics.py
+++ ssc_scoring/compute_metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import myutil.myutil as futil
 
 from ssc_scoring.mymodules.confusion_test import confusion, read_check
 
@@ -47,9 +48,15 @@ def metrics(pred_fpath: str, label_fpath: str, bland_in_1: bool, adap_markersize
 
 
 if __name__ == "__main__":
-    pred_fpath = "/data/jjia/ssc_scoring/ssc_scoring/dataset/observer_agreement/16_patients/LKT2_16patients.csv"
+    # pred_fpath = "/data/jjia/ssc_scoring/ssc_scoring/dataset/observer_agreement/16_patients/LKT2_16patients.csv"
     # pred_fpath = "/data/jjia/ssc_scoring/ssc_scoring/results/models/1405_1404_1411_1410/16pats_pred.csv"
-    label_fpath = "/data/jjia/ssc_scoring/ssc_scoring/dataset/observer_agreement/16_patients/ground_truth_16patients.csv"
-    bland_in_1 = False
-    adap_markersize = True
+    # label_fpath = "/data/jjia/ssc_scoring/ssc_scoring/dataset/observer_agreement/16_patients/ground_truth_16patients.csv"
+
+    pred_fpath = "/data/jjia/ssc_scoring/ssc_scoring/results/models_pos/193/test_pred.csv"
+    label_fpath = "/data/jjia/ssc_scoring/ssc_scoring/results/models_pos/193/test_label.csv"
+
+    bland_in_1 = True
+    adap_markersize = False
     metrics(pred_fpath, label_fpath, bland_in_1, adap_markersize)
+    icc = futil.icc(label_fpath, pred_fpath)
+    print('icc:', icc)
